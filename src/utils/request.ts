339246a618f437229939f26d1f1ac832a0b8b366
浮瑞欣 <545397177@qq.com>
2023-08-24 15:37:17 +0800

    feat: 请求工具函数封装

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -51,5 +51,18 @@ instance.interceptors.response.use(
   }
 )
 
+type Data<T> = {
+  code: number
+  message: string
+  data: T
+}
+// 4. 请求工具函数
+const request = <T>(url: string, method = 'get', submitData?: object) => {
+  return instance.request<T, Data<T>>({
+    url,
+    method,
+    [method.toLowerCase() === 'get' ? 'params' : 'data']: submitData
+  })
+}
 //  导出baseURL的目的是其他模块可能需要使用
-export { baseURL, instance }
+export { baseURL, instance, request }
