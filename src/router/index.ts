76f052412f0dd36eb1e9a21687ab0ecc63d232a5
浮瑞欣 <545397177@qq.com>
2023-08-25 11:17:16 +0800

    feat: vant按需引入

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import index from '@/views/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: index
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login/index.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Register/index.vue')
     }
   ]
 })
