0a4a0ef8400917502c0ee61532a2483b0307ffe7
浮瑞欣 <545397177@qq.com>
2023-08-25 16:30:32 +0800

    feat: 打包svg地图

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores'
+
+// 打包svg地图
+import 'virtual:svg-icons-register'
+
 // 样式全局使用
 import 'vant/lib/index.css'
 import './styles/main.scss'
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 按需引入vant
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
+
+// 根据 icons 文件svg图片打包到项目中，通过组件使用图标
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +20,10 @@ export default defineConfig({
       dts: false,
       // 原因：Toast Confirm 这类组件的样式还是需要单独引入，样式全局引入了，关闭自动引入
       resolvers: [VantResolver({ importStyle: false })]
+    }),
+    createSvgIconsPlugin({
+      // 指定图标文件夹，绝对路径（NODE代码）
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')]
     })
   ],
   resolve: {
